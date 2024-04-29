04ac2e3a222d56492c61deb47faf0cb236ed0bc2
John Doe <johndoe@example.com>
2024-04-29 21:34:37 +0100

    UPDATE

--- projetpy.py
+++ projetpy.py
@@ -48,32 +48,71 @@ def ajout():
 
 def supprim():
     name = fen.num_supprim.text()
-    if (not name.isalpha()) or (name==""):
+    if not name.isalpha() or name == "":
         QMessageBox.critical(fen, "Error", "Vérifiez votre nom")
         fen.num_supprim.setText("")
         fen.num_supprim.setFocus()
-    else :
-        tous_les_contact = [nom for nom in csv.reader(open("projetpython.csv"))]
-        trouve = False
-        index = 0
-        for contact in tous_les_contact:
-            if name == contact[0]:
-                trouve = True
-                tous_les_contact = tous_les_contact[:index] + tous_les_contact[index+1:]
-                break
-            index += 1
-        with open('projetpython.csv','w') as file:
-            csv_writer = csv.writer(file)
-            for contact in tous_les_contact:
-                csv_writer.writerow(contact)
-        QMessageBox.information(fen, "Cool", "suppression effectuer avec succes")
-        fen.num_supprim.clear()
+    else:
+        liste = []
+        test = False
+        f = open("projetpython.csv", "r", newline='')
+        file = csv.reader(f)
+        for ligne in file:
+            if name != ligne[0]:
+                liste.append(ligne)
+            else:
+                test = True
+        f.close()
 
+        if not test:
+            error(test)
+            return
+
+        f = open("projetpython.csv", "w", newline='')
+        file = csv.writer(f)
+        for ligne in liste:
+            file.writerow(ligne)
+        f.close()
+
+        QMessageBox.information(None, "Notice", "Suppression avec succès")
+
+def error(found):
+    if not found:
+        QMessageBox.critical(None, "Error", "NOM INEXISTANT")
+
+
+def modification():
+    nom = fen.num_modif.text()
+    new_mail = fen.nouv_mail.text()
+    new_tel = fen.nouv_num.text()
+    
+    d = []
+    found = False
+    with open("projetpython.csv","r") as f:
+        file = csv.reader(f)
+        for data in file:
+            if data[0] == nom:
+                data[1] = new_mail
+                data[2] = new_tel
+                found = True
+            d.append(data)
+    
+    if not found:
+        error(found)
+        return
+    
+    with open("projetpython.csv","w",newline='') as f:
+        file = csv.writer(f)
+        for data in d:
+            file.writerow(data)
+            
+    QMessageBox.information(None,"Notice","Modification effectuée avec succes")
+    
 
 def aff():
     name=fen.num_aff.text()
     fen.table.setRowCount(0)
-    for contact in  open ('projetpython.csv','r'):
+    for contact in open('projetpython.csv','r'):
         [nom,email,tel] = contact.split(',')
         if(name==nom):
             row = fen.table.rowCount()
@@ -81,10 +120,20 @@ def aff():
             fen.table.setItem(row,0,QTableWidgetItem(nom))
             fen.table.setItem(row,1,QTableWidgetItem(email))
             fen.table.setItem(row,2,QTableWidgetItem(tel.strip()))
+            
     fen.num_aff.setText("")
-
+    
         
-        
+def afftout():
+    fen.table.setRowCount(0)
+    for contact in open("projetpython.csv","r"):
+        [nom,email,tel] = contact.split(',')
+        row = fen.table.rowCount()
+        if nom != "nom" and email != "mail" and tel != "tel":
+            fen.table.insertRow(row)
+            fen.table.setItem(row,0,QTableWidgetItem(nom))
+            fen.table.setItem(row,1,QTableWidgetItem(email))
+            fen.table.setItem(row,2,QTableWidgetItem(tel.strip()))
         
 
 app = QApplication([])
@@ -93,5 +142,6 @@ fen.show()
 fen.ajouter.clicked.connect(ajout)
 fen.supprimer.clicked.connect(supprim)
 fen.afficher.clicked.connect(aff)
-
+fen.modifier.clicked.connect(modification)
+fen.afficher_t.clicked.connect(afftout)
 app.exec_()
